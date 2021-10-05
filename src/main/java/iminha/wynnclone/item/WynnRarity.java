e0c5033b551326364072f1b824d35b3cf49f1595
Iminha <justinschrier@gmail.com>
2021-10-04 17:25:30 -0700

    Rarity cleanup

--- src/main/java/iminha/wynnclone/item/WynnRarity.java
+++ src/main/java/iminha/wynnclone/item/WynnRarity.java
@@ -7,21 +7,21 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 
 public enum WynnRarity {
-    DEPRESSING(ChatFormatting.GRAY, new TranslatableComponent("wynn.tooltip.rarity.depressing"), 0, 0),
-    NORMAL(ChatFormatting.WHITE, new TranslatableComponent("wynn.tooltip.rarity.normal"), 1, 1),
-    UNIQUE(ChatFormatting.YELLOW, new TranslatableComponent("wynn.tooltip.rarity.unique"), 2, 2),
-    RARE(ChatFormatting.LIGHT_PURPLE, new TranslatableComponent("wynn.tooltip.rarity.rare"), 3,3 ),
-    LEGENDARY(ChatFormatting.AQUA, new TranslatableComponent("wynn.tooltip.rarity.legendary"), 4, 4),
-    FABLED(ChatFormatting.RED, new TranslatableComponent("wynn.tooltip.rarity.fabled"), 5, 5),
-    MYTHIC(ChatFormatting.DARK_PURPLE, new TranslatableComponent("wynn.tooltip.rarity.mythic"), 6, 6),
-    SET(ChatFormatting.GREEN, new TranslatableComponent("wynn.tooltip.rarity.set"), -1, 7); //unused
+    DEPRESSING(ChatFormatting.GRAY, "wynn.tooltip.rarity.depressing", 0, 0),
+    NORMAL(ChatFormatting.WHITE, "wynn.tooltip.rarity.normal", 1, 1),
+    UNIQUE(ChatFormatting.YELLOW, "wynn.tooltip.rarity.unique", 2, 2),
+    RARE(ChatFormatting.LIGHT_PURPLE, "wynn.tooltip.rarity.rare", 3,3 ),
+    LEGENDARY(ChatFormatting.AQUA, "wynn.tooltip.rarity.legendary", 4, 4),
+    FABLED(ChatFormatting.RED, "wynn.tooltip.rarity.fabled", 5, 5),
+    MYTHIC(ChatFormatting.DARK_PURPLE, "wynn.tooltip.rarity.mythic", 6, 6),
+    SET(ChatFormatting.GREEN, "wynn.tooltip.rarity.set", -1, 7); //unused
 
     private final ChatFormatting color;
-    private final TranslatableComponent tooltip;
+    private final String tooltip;
     private final int maxModifiers;
     private final int id;
 
-    WynnRarity(ChatFormatting color, TranslatableComponent tooltip, int maxModifers, int id) {
+    WynnRarity(ChatFormatting color, String tooltip, int maxModifers, int id) {
         this.color = color;
         this.tooltip = tooltip;
         this.maxModifiers = maxModifers;
@@ -33,12 +33,11 @@ public enum WynnRarity {
     }
 
     public Component getTooltip() {
-        MutableComponent coloredTooltip = (new TextComponent("")).append(
+        Component translatedTooltip = new TranslatableComponent(tooltip);
+        return (new TextComponent("")).append(
                 getColor().toString()
-                + tooltip.getString()
+                + translatedTooltip.getString()
                 + ChatFormatting.RESET);
-
-        return coloredTooltip;
     }
     public int getMaxModifiers() {return maxModifiers;}
     public int getId() {return this.id;}
